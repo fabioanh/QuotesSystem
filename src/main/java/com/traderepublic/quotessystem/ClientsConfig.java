d46529af4d2cc4d08534446dd3985c6ccd1b87f5
Fabio Navarrete <fabioanh@gmail.com>
2020-06-19 21:24:11 +0400

    Cleaning a bit the unnecessary information

--- src/main/java/com/traderepublic/quotessystem/ClientsConfig.java
+++ src/main/java/com/traderepublic/quotessystem/ClientsConfig.java
@@ -1,18 +1,10 @@
 package com.traderepublic.quotessystem;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.traderepublic.quotessystem.clients.InstrumentMessageHandler;
 import com.traderepublic.quotessystem.clients.QuoteMessageHandler;
-import org.bson.codecs.configuration.CodecRegistries;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -26,17 +18,6 @@ import java.net.URI;
 public class ClientsConfig {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ClientsConfig.class);
-
-    //    @Value("${elasticsearch.host:localhost}")
-    @Value("${mongodb.host:localhost}")
-    private String host;
-
-    //    @Value("${elasticsearch.port:9200}")
-    @Value("${mongodb.port:9200}")
-    private String port;
-
-    @Value("${mongodb.uri:mongodb://localhost:27017}")
-    private String mongoURI;
 
     @Autowired
     private InstrumentMessageHandler instrumentMessageHandler;
@@ -54,20 +35,6 @@ public class ClientsConfig {
         return webSocketClient("instruments");
     }
 
-    @Bean(destroyMethod = "close")
-    public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString(mongoURI);
-        CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build());
-        CodecRegistry codecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
-
-        MongoClientSettings clientSettings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .codecRegistry(codecRegistry)
-                .build();
-        return MongoClients.create(clientSettings);
-//        return new MongoClient(host, Integer.valueOf(port));
-
-    }
 
     private WebSocketClient webSocketClient(String streamId) {
         final String url = String.format("ws://localhost:8080/%s", streamId);
---
+++ src/main/java/com/traderepublic/quotessystem/DatabaseConfig.java
@@ -0,0 +1,38 @@
+package com.traderepublic.quotessystem;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DatabaseConfig {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(DatabaseConfig.class);
+
+    @Value("${mongodb.uri:mongodb://localhost:27017}")
+    private String mongoURI;
+
+    @Bean(destroyMethod = "close")
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoURI);
+        CodecRegistry pojoCodecRegistry = CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        CodecRegistry codecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+
+        MongoClientSettings clientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .codecRegistry(codecRegistry)
+                .build();
+        return MongoClients.create(clientSettings);
+
+    }
+}
